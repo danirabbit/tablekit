10e24d49edf146a17f71bd43d298f9a329cf670a
Daniel Foré <daniel@elementary.io>
2016-10-23 12:34:28 -0700

    add recipe card

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(function () {
-    var recipe_entry = $('input[name=recipe_name]');
+    var recipe_entry = $("input[name=recipe_name]");
+    var recipe_title = $(".title");
+
+    $(".cost span").text("4.86");
 
     if (localStorage.getItem("recipe_name") != "undefined") {
         recipe_entry.val(localStorage.getItem("recipe_name"));
+        recipe_title.text(localStorage.getItem("recipe_name"));
     }
 
     recipe_entry.change(function() {
         localStorage.setItem("recipe_name", recipe_entry.val());
-        console.log (localStorage.getItem("recipe_name"));
+        recipe_title.text(localStorage.getItem("recipe_name"));
     });
 
 });
