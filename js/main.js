667f2ec0ef6dcddd7487e450fc513fb2b38e7a1f
Daniel Foré <daniel@elementary.io>
2016-10-23 21:06:22 -0700

    move card to a partial, rearrange js, comment dead ui

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
 $(function () {
-    var recipe_title = $(".title");
 
-    $(".cost span").text("4.86");
-
-    if (localStorage.getItem("recipe_name") != "undefined") {
-        recipe_title.text(localStorage.getItem("recipe_name"));
-    }
-
-    $(".card").click(function() {
-        window.location.href = "edit";
-    });
 });
---
+++ js/recipes.js
@@ -0,0 +1,17 @@
+$(function () {
+    $.get("partials/card.html", function(data){
+        $(".recipe-list").html(data);
+
+        var recipe_title = $(".title");
+
+        $(".cost span").text("4.86");
+
+        if (localStorage.getItem("recipe_name") != "undefined") {
+            recipe_title.text(localStorage.getItem("recipe_name"));
+        }
+
+        $(".card").click(function() {
+            window.location.href = "edit";
+        });
+    });
+});
